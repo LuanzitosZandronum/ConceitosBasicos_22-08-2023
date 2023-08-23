7e8a4e90aaf43f312a3d735f04e917fac9143537
LuanzitosZandronum <142948580+LuanzitosZandronum@users.noreply.github.com>
2023-08-22 21:11:23 -0300

    Add files via upload

---
+++ ConceitosBasicos/CalculadoraIMC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosBasicos
+{
+    public class CalculadoraIMC
+    {
+        public double IMC(double altura, double peso)
+        {
+            return peso / (altura*altura);
+        }
+    }
+}
